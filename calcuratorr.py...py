55e7d5a039f3f54478ed584fa27ffcbe75ee7d40
Ganesh bhosale <89768744+Ganesh0221@users.noreply.github.com>
2021-12-25 06:36:31 +0530

    Add files via upload

---
+++ calcuratorr.py...py
@@ -0,0 +1,39 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/GANU/Desktop/calculaterr.py ================
+enter a number:-2
+enter a number:-3
+enter operation:-+
+2+3=5
+>>> 
+================= RESTART: C:/Users/GANU/Desktop/calculaterr.py ================
+enter a number:-8
+enter a number:-6
+enter operation:--
+8-6=2
+>>> 
+================= RESTART: C:/Users/GANU/Desktop/calculaterr.py ================
+enter a number:-3
+enter a number:-5
+enter operation:-*
+3*5=15
+>>> 
+================= RESTART: C:/Users/GANU/Desktop/calculaterr.py ================
+enter a number:-6
+enter a number:-3
+enter operation:-/
+6/3=2.0
+>>> 
+================= RESTART: C:/Users/GANU/Desktop/calculaterr.py ================
+enter a number:-8
+enter a number:-2
+enter operation:-//
+8//2=4
+>>> 
+================= RESTART: C:/Users/GANU/Desktop/calculaterr.py ================
+enter a number:-6
+enter a number:-2
+enter operation:-%
+6%2=0
+>>> 
